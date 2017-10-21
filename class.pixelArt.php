04fe0818775a882ba6c1426fcc34563c46290f78
golendercaria <golendercaria@golendercaria-2.local>
2017-10-21 19:19:16 +0200

    Update detection of pixel > limit of image (substract 1 of width and height). Add methode for making shape and making fusion image.

--- class.pixelArt.php
+++ class.pixelArt.php
@@ -27,8 +27,7 @@
 				if( $this->imageRessource ){
 			    	$this->imageWidth 	= imagesx( $this->imageRessource );
 			    	$this->imageHeight 	= imagesy( $this->imageRessource );
-			    	
-			    	pre($this);
+
 				}else{
 					echo "Image not loading";
 				}
@@ -43,15 +42,19 @@
 			}
 			
 			$currentPoint = 0;
+			$this->listOfPoint = array();
 			while( $currentPoint < $this->params["nbrPoint"] ){
 				$currentPoint++;
 				
-				$tmpX = rand(0,$this->imageWidth);
-				$tmpY = rand(0,$this->imageHeight);
+				$tmpX = rand(0,$this->imageWidth-1);
+				$tmpY = rand(0,$this->imageHeight-1);
 				
 				$tmpPixel = $this->getPixelColor($tmpX, $tmpY);
-				
-				pre( $tmpPixel );
+				$this->listOfPoint[] = array(
+					"x" => $tmpX, 
+					"y"	=> $tmpY, 
+					"color" => $tmpPixel
+				);
 			}
 			
 		}
@@ -60,15 +63,82 @@
 			$rgb = imagecolorat($this->imageRessource, $x, $y);
 			return imagecolorsforindex($this->imageRessource, $rgb);
 		}
+		
+		public function makingShape(){
+			
+			//check if sufficient point
+			if( !isset($this->params["shape"]) ){
+				die("Not shape defined");
+			}
+			
+			$this->{$this->params["shape"]}();
+		}
+		
+		public function generateFusionImage(){
+			$tmpImage  	= imagecreatetruecolor($this->imageWidth, $this->imageHeight);
+			$whiteBg 	= imagecolorallocate($tmpImage, 255, 255, 255);
+			imagefill($tmpImage, 0, 0, $whiteBg);
+			
+			return $tmpImage;
+		}
+		
+		public function rect(){
+			
+			//check if point exist
+			if( empty($this->listOfPoint) ){
+				die("Empty listOfPoint");
+			}
+			
+			//check if range for size of shape
+			if( !isset($this->params["rangeSizeShape"][0]) || !isset($this->params["rangeSizeShape"][1]) ){
+				die("Missig range size shape");	
+			}
+			
+			//generate blank image 
+			$tmpImage = $this->generateFusionImage();
+			
+			//making shape for each point
+			foreach($this->listOfPoint as $point){
+
+				//generate random size and get the middle of shape
+				$sizeShape 		= rand($this->params["rangeSizeShape"][0], $this->params["rangeSizeShape"][1]);
+				$middleOfShape 	= round($sizeShape/2);
+				
+				//calculate the position of begin shape
+				$shapeX = $point["x"] - $middleOfShape;
+				$shapeY = $point["y"] - $middleOfShape;
+				
+				//calculate end position of shape
+				$shapeXEnd = $shapeX + $sizeShape;
+				$shapeYEnd = $shapeY + $sizeShape;
+
+				//prepare color
+				$tmpColor = imagecolorallocatealpha($tmpImage, $point["color"]["red"], $point["color"]["green"], $point["color"]["blue"], $point["color"]["alpha"]);
+				//construct shape on tmpImage
+				imagefilledrectangle($tmpImage, $shapeX, $shapeY, $shapeXEnd, $shapeYEnd, $tmpColor);
+					
+			}
+			
+			imagepng($tmpImage, "image_created/rect.png");
+			imagedestroy($tmpImage);
+			
+			?><img src="image_created/rect.png?<?php uniqid(); ?>" /><?php
+		}
 
 	}
 	
 	//params for class
 	$params = array(
-		"fileName"  => "pikachu.jpg",
-		"nbrPoint"	=> 5,
+		"fileName"            => "pikachu.jpg",
+		"nbrPoint"            => 5,
+		"shape"               => "rect",
+		"rangeSizeShape"	  => array(10,30)
 	);
 	
 	//launch object	
 	$imageRendering = new pixelArt($params);
 	$imageRendering->collectPixel();
+	$imageRendering->makingShape();
+	
+	
+	
