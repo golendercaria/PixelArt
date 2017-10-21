36c8ae17bbcba3ebd899bf423a5b899d1b4a4547
golendercaria <golendercaria@golendercaria-2.local>
2017-10-21 17:53:40 +0200

    Get properties of img

--- class.pixelArt.php
+++ class.pixelArt.php
@@ -1,6 +1,14 @@
 <?php
 
+	ini_set('memory_limit', '2048M');
 
+	//error_reporting(0);
+	function pre($a = null){
+		echo "<pre>";
+		print_r($a);
+		echo "<pre>";
+	}
+	
 	class pixelArt{
 		
 		private $pathImage = "image/";
@@ -8,13 +16,21 @@
 		public function __construct( $params = array() ){
 			if( isset($params["fileName"]) && $params["fileName"] != null){
 				//get image
-				$this->url = $this->pathImage . $params["fileName"];
-			    $image = imagecreatefromjpeg( $this->url );
-
-				var_dump($image);
+				$this->url 				= $this->pathImage . $params["fileName"];
+				$this->imageRessource 	= imagecreatefromjpeg( $this->url );
+				
+				//save height and width of image
+				if( $this->imageRessource ){
+			    	$this->imageWidth 	= imagesx( $this->imageRessource );
+			    	$this->imageHeight 	= imagesy( $this->imageRessource );
+			    	
+			    	pre($this);
+				}else{
+					echo "Image not loading";
+				}
 			}
 		}
-		
+
 	}
 	
 	//params for class
